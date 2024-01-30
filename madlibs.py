e4c5e9735ab34502bd1319a6e8390ce086cc5d63
SirPopGhost <srikardevesetti@gmail.com>
2024-01-29 16:22:29 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,17 @@
-# TODO Create your inputs, variables, and add your story here!
+noun = input("Please enter a noun. ")
+verb = input("Please enter a verb. ")
+adjective = input("Please enter a adjective. ")
+
+print(f"The {noun} is {verb} with a {adjective} face.")
+
+input1 = input("Please enter input here. ")
+input2 = input("Please enter input here. ")
+input3 = input("Please enter input here. ")
+input4 = input("Please enter input here. ")
+input5 = input("Please enter input here. ")
+input6 = input("Please enter input here. ")
+input7 = input("Please enter input here. ")
+input8 = input("Please enter input here. ")
+input9 = input("Please enter input here. ")
+input10 = input("Please enter input here. ")
+
